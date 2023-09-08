198f0abdd94b6c2d2003373985972a9cd8a8d63a
Udit Narayan Ray <91251041+Udit-Narayan-Ray@users.noreply.github.com>
2023-09-08 20:37:41 +0530

    first commit of hibernate project

---
+++ hibernate/src/main/java/com/hibernate/hibernate/App.java
@@ -0,0 +1,28 @@
+package com.hibernate.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+//        System.out.println( "Hello World!" );
+    	table_model m=new table_model();
+    	m.setName("jsk");
+    	m.setRn(1);
+    	
+    	Configuration con=new Configuration().configure("config.cfg.xml").addAnnotatedClass(table_model.class);
+    	SessionFactory sf=con.buildSessionFactory();
+    	Session session=sf.openSession();
+    	Transaction  tran=session.beginTransaction();
+    	session.save(m);
+    	tran.commit();
+    }
+}
