828796ba2b11ea283903f86145fb48b2d2452d9c
Udit Narayan Ray <91251041+Udit-Narayan-Ray@users.noreply.github.com>
2023-09-08 20:52:43 +0530

    deleted 1st element from table

--- hibernate/src/main/java/com/hibernate/hibernate/App.java
+++ hibernate/src/main/java/com/hibernate/hibernate/App.java
@@ -17,15 +17,13 @@ public class App
     	table_model m=new table_model();
     	m.setName("jsk");
     	m.setRn(1);
-    	table_model n=new table_model();
-    	n.setRn(2);
-    	n.setName("RKU");
+    	
     	Configuration con=new Configuration().configure("config.cfg.xml").addAnnotatedClass(table_model.class);
     	SessionFactory sf=con.buildSessionFactory();
     	Session session=sf.openSession();
     	Transaction  tran=session.beginTransaction();
-    	session.save(n);
-    	
+//    	session.save(m);
+    	session.delete(m);
     	tran.commit();
     }
 }
