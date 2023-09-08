a942dce46a279ce83818e8490c7ce4cb84d25a2f
Udit Narayan Ray <91251041+Udit-Narayan-Ray@users.noreply.github.com>
2023-09-08 22:58:20 +0530

    commiting to revert to delete all variable

--- hibernate/src/main/java/com/hibernate/hibernate/table_model2.java
+++ hibernate/src/main/java/com/hibernate/hibernate/table_model2.java
@@ -3,8 +3,5 @@ package com.hibernate.hibernate;
 public class table_model2 {
 	private int slno;
 	private String name;
-<<<<<<< HEAD
-	
-=======
->>>>>>> parent of 0117b35 (saving comment line)
+
 }
