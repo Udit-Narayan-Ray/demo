6ce0fc47da770211f33c91a2dcf49e07af8c3a03
Udit Narayan Ray <91251041+Udit-Narayan-Ray@users.noreply.github.com>
2023-09-09 07:21:12 +0530

    added get set for the slno in table 2 class

--- hibernate/src/main/java/com/hibernate/hibernate/table_model2.java
+++ hibernate/src/main/java/com/hibernate/hibernate/table_model2.java
@@ -4,5 +4,14 @@ public class table_model2 {
 
 	private int slno;
 	private String name;
+	
+	public void setSlno(int slno)
+	{
+		this.slno=slno;
+	}
+	public int getSlno()
+	{
+		return this.slno;
+	}
 
 }
